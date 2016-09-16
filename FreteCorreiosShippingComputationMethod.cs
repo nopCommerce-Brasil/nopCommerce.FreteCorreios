af29793d71685649d4407a3f4fda1ef847836acd
Ricardo Fiori <rfiori44@gmail.com>
2016-09-15 23:51:12 -0300

    Update

--- FreteCorreiosShippingComputationMethod.cs
+++ FreteCorreiosShippingComputationMethod.cs
@@ -7,12 +7,30 @@ using Nop.Core.Plugins;
 using Nop.Services.Shipping;
 using Nop.Services.Shipping.Tracking;
 using System.Web.Routing;
+using Nop.Services.Logging;
+using Nop.Services.Configuration;
+using FreteCorreios;
+using Nop.Core.Domain.Shipping;
 
 namespace Nop.Plugin.Shipping.FreteCorreios
 {
     public class FreteCorreiosShippingComputationMethod : BasePlugin, IShippingRateComputationMethod
     {
-        public ShippingRateComputationMethodType ShippingRateComputationMethodType
+        //private readonly ICurrencyService _currencyService;
+        private readonly FreteCorreiosSetting _FreteCorreiosSetting;
+        private readonly ILogger _logger;
+        private readonly ISettingService _settingService;
+        private readonly IShippingService _shippingService;
+
+        public FreteCorreiosShippingComputationMethod(FreteCorreiosSetting freteCorreiosSetting, ILogger logger, ISettingService settingServices, IShippingService shippingServices)
+        {
+            _FreteCorreiosSetting = freteCorreiosSetting;
+            _logger = logger;
+            _settingService = settingServices;
+            _shippingService = shippingServices;
+    }
+
+    public ShippingRateComputationMethodType ShippingRateComputationMethodType
         {
             get
             {
@@ -34,7 +52,46 @@ namespace Nop.Plugin.Shipping.FreteCorreios
 
         public GetShippingOptionResponse GetShippingOptions(GetShippingOptionRequest getShippingOptionRequest)
         {
-            throw new NotImplementedException();
+            if (getShippingOptionRequest == null)
+                throw new ArgumentNullException("getShippingOptionRequest");
+
+            if (getShippingOptionRequest.Items == null)
+                return new GetShippingOptionResponse { Errors = new List<string> { "Sem itens poara envioNo." } };
+
+            if (getShippingOptionRequest.ShippingAddress == null)
+                return new GetShippingOptionResponse { Errors = new List<string> { "Endereço não foi informado." } };
+
+            //if (getShippingOptionRequest.ShippingAddress.Country == null)
+            //    return new GetShippingOptionResponse { Errors = new List<string> { "País não foi Shipping country is not set" } };
+
+            if (string.IsNullOrEmpty(getShippingOptionRequest.ZipPostalCodeFrom))
+                return new GetShippingOptionResponse { Errors = new List<string> { "CEP de origem não foi informado." } };
+
+            //create object for the get rates requests
+            var result = new GetShippingOptionResponse();
+            //get rate for all available services
+            var errorSummary = new StringBuilder();
+
+            //write errors
+            var errorString = errorSummary.ToString();
+
+            var frete = new FreteCorreiosWS().CalcPrecoAsync("", "", "41106", getShippingOptionRequest.ZipPostalCodeFrom, getShippingOptionRequest.ShippingAddress.ZipPostalCode, getShippingOptionRequest.Items[0].ShoppingCartItem.Product.Weight.ToString(), 1, 16, 5, 11, 0, "N", getShippingOptionRequest.Items[0].ShoppingCartItem.Product.Price, "N");
+
+            result.ShippingOptions.Add(new ShippingOption
+            {
+                Name = "PAC",
+                Rate = 0,
+                 Description = frete.Result.Servicos.ToList().ToString()
+                });         
+
+
+            if (!string.IsNullOrEmpty(errorString))
+                _logger.Error(errorString);
+
+            if (!result.ShippingOptions.Any())
+                result.AddError(errorString);
+
+            return result;
         }
 
         public IShipmentTracker ShipmentTracker
